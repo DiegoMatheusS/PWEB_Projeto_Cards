0c3ae8e6426c4de6d8c5fe92df83ac945e79e9f1
Diego Matheus <dms.diego.2019@gmail.com>
2024-04-18 22:11:42 -0300

    feat: adicionado rotas e botoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './template/header/header.component';
 import { FooterComponent } from './template/footer/footer.component';
 import { HomeComponent } from './components/home/home.component';
 
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import { registerLocaleData } from '@angular/common';
+import { ListarProdutosComponent } from './components/produtos/listar-produtos/listar-produtos.component';
+
+registerLocaleData(localePt)
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ListarProdutosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    {provide : LOCALE_ID, useValue : 'pt-BR' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,19 +7,22 @@ import { Component } from '@angular/core';
 })
 export class HomeComponent {
 
-  nomeProduto: string = "Honda City";
+  nomeProduto: string = "Curso de Angular";
   anuncio: string = `O ${this.nomeProduto} está em promoção` ;
-  idProduto: number = 123;
-  precoProduto: number = 80.000;
+  idProduto: number = 3;
+  precoProduto: number = 2.51;
   promocao: boolean = true;
+  foto : string = 'assets/crud.png';
+  dataValidade = "2021-12-31"
 
   constructor () {
    // this.anuncio = "O" + this.nomeProduto + "Esta em Promoção"; com concatenação
     console.log("Nome do Produto:", this.nomeProduto);
     console.log("Anuncio:", this.anuncio);
     console.log("ID:", this.idProduto);
-    console.log("Preço:", this.precoProduto.toFixed(3));
+    console.log("Preço:", this.precoProduto);
     console.log("Promoção:", this.promocao);
+
 
     // escopo das variaveis dentro do código
     var idade = 23
@@ -31,16 +34,16 @@ export class HomeComponent {
     /* var varivel1;
     let variavel2;
     const variavel3 = 1;
-    */
 
-   /* function imprimeIDadeVar()
+
+    function imprimeIDadeVar()
     {
       for (var idade = 30; idade <=40; idade ++) {
         console.log("Idade dentro do for: ", idade)
       }
       console.log("Idade fora do for: ",idade)
     }
-    imprimeIDade()*/
+    imprimeIDade()
 
     function imprimeIDade()
     {
@@ -52,6 +55,50 @@ export class HomeComponent {
     imprimeIDade()
 
 
-  }
+    var a = 10;
+    let b = 20;
+    function bar() {
+      var a = 30;
+      let b = 40;
+      if(true){
+        var a = 50;
+        let b = 60;
+        console.log('Variavel a dentro do if: ', a);
+        console.log('Variavel b dentro do if: ', b);
+
+      }
+      console.log('Variavel a dentro da funcao: ', a);
+      console.log('Variavel ab dentro da funcao: ', b);
+    }
+    bar();
+    console.log('Variavel a dentro da fora funcao: ', a);
+    console.log('Variavel ab dentro da fora funcao: ', b);
+
+    let a =1;
+    function bar(){
+      console.log('variavel a; ', a);
+      let b = 2;
+      if(true) {
+        let c= 3;
+        let a = 4;
+        console.log('variavel a dentro do if', a)
+        console.log('variavel b dentro do if', b);
+        console.log('variavel c dentro do if', c);
+      }
+      console.log('variavel a fora do if', a);
+      console.log('variavel b fora do if', b);
+    }
+    bar();*/
+    var a = 5;
+    var b = 10;
+    if(a = 5){
+     let a =4;
+     var b = 1;
+     console.log("a é igual", a);
+     console.log("b é igual", b);
+    }
+    console.log("fora a é igual", a);
+    console.log("fora b é igual", b);
+   }
 
 }
